9fed667e2de4a06727d32e6a55cb3c6a9221384a
0xjaqbek <jaqbek.eth@gmail.com>
2025-04-10 16:14:54 +0200

    path of images

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -15,7 +15,7 @@ const products = [
     price: 47, 
     description: "Manifest Your Reality",
     stripeId: "buy_btn_1QOKLnK1N5l6JY7eOroi5V75",
-    image: "/images/olive.png" // Updated to match your file
+    image: "images/olive.png" // Updated to match your file
   },
   { 
     id: 2, 
@@ -25,7 +25,7 @@ const products = [
     price: 47, 
     description: "Create Your Perfect World",
     stripeId: "buy_btn_1QOKNFK1N5l6JY7e6zEMyXOG",
-    image: "/images/olive1.png" // Updated to match your file
+    image: "images/olive1.png" // Updated to match your file
   },
   { 
     id: 3, 
@@ -35,7 +35,7 @@ const products = [
     price: 47, 
     description: "Embrace Pure Energy",
     stripeId: "buy_btn_1QSG0JK1N5l6JY7ehIiH2UrZ",
-    image: "/images/white.png" // Updated to match your file
+    image: "images/white.png" // Updated to match your file
   },
   { 
     id: 4, 
@@ -45,7 +45,7 @@ const products = [
     price: 47, 
     description: "Pure Light Within",
     stripeId: "buy_btn_1QSFtuK1N5l6JY7eVuMSXjGE",
-    image: "/images/white1.png" // Updated to match your file
+    image: "images/white1.png" // Updated to match your file
   },
 ];
 
